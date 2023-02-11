70d9fef3886c5decbdfb3ff93b6fa662489eb621
CristianC118 <98475802+CristianC118@users.noreply.github.com>
2023-02-10 23:32:54 -0300

    Agregación de Función Resta

--- codigo/ejemplo_Git_datos.py
+++ codigo/ejemplo_Git_datos.py
@@ -13,4 +13,10 @@ def suma():
 
 def resta():
 
-  pass
+  numero1 = int(input('Introduce el primer número: '))
+  numero2 = int(input('Introduce el segundo número: '))
+  
+  print(numero1 - numero2)
+
+
+#------------------------------------------------------
