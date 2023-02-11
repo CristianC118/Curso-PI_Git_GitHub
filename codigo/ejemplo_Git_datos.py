a43850d126494479c2003a74677b616bd0bf38f1
Cristian <cristiancristaldo118@gmail.com>
2023-02-10 23:39:30 -0300

    Arreglos

--- codigo/ejemplo_Git_datos.py
+++ codigo/ejemplo_Git_datos.py
@@ -15,8 +15,11 @@ def resta():
 
   numero1 = int(input('Introduce el primer número: '))
   numero2 = int(input('Introduce el segundo número: '))
-  
+
   print(numero1 - numero2)
 
 
 #------------------------------------------------------
+
+
+# --
