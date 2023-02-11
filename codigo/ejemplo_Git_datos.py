4cc2fac88d36f5071d6b0cd88f095368d33262eb
Cristian <cristiancristaldo118@gmail.com>
2023-02-10 23:15:28 -0300

    Comienzo

---
+++ codigo/ejemplo_Git_datos.py
@@ -0,0 +1,13 @@
+
+
+def suma():
+
+  numero1 = int(input('Introduce el primer número: '))
+  numero2 = int(input('Introduce el segundo número: '))
+
+  print(numero1 + numero2)
+
+
+#------------------------------------------------------
+
+
