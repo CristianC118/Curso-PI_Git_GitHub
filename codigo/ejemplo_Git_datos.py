20b3c2c37f246606aa46c5747e60dfd8d7a1c38e
Cristian <cristiancristaldo118@gmail.com>
2023-02-14 17:03:11 -0300

    Modificando script python desde D2

--- codigo/ejemplo_Git_datos.py
+++ codigo/ejemplo_Git_datos.py
@@ -33,3 +33,9 @@ def multi():
 #-----------------------------------------------------
 
 
+def division():
+
+  numero1 = int(input('Introduce el primer número: '))
+  numero2 = int(input('Introduce el segundo número: '))
+
+  print(numero1 / numero2)
