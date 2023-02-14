8eb85f10144312a8bca973e3ee748f6511b4f49e
Cristian <cristiancristaldo118@gmail.com>
2023-02-14 14:41:28 -0300

    agregado nueva función

--- codigo/ejemplo_Git_datos.py
+++ codigo/ejemplo_Git_datos.py
@@ -22,4 +22,14 @@ def resta():
 #------------------------------------------------------
 
 
-# --
+def multi():
+
+  numero1 = int(input('Introduce el primer número: '))
+  numero2 = int(input('Introduce el segundo número: '))
+
+  print(numero1 * numero2)
+
+
+#-----------------------------------------------------
+
+
